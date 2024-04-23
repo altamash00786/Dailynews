07d17e37c876411ef4f8904ba91bf0d0af48e2a0
Shahista Shaikh <shahistashaikh000786@gmail.com>
2024-04-23 17:31:16 +0530

    pushed

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
-import { Link } from "react-router-dom";
 
 export class NewsItems extends Component {
     render() {
         let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
         return (
-            <div className="my-4">
+            <div className="my-3">
                 <div className="card">
-                    <span class="position-absolute top-0  translate-middle badge rounded-pill bg-success" style={{ left: '82%', zIndex: '1' }}>{source}</span>
-                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." style={{ width: '263px', height: '200px' }} />
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}> {source}
+                        </span>
+                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." />
                     <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
+                        <h5 className="card-title">{title}  </h5>
                         <p className="card-text">{description}</p>
-                        <p class="card-text"><small class="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
-                        <Link rel="noreferrer" to={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More...</Link>
+                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -4,7 +4,7 @@ export default class Spinner extends Component {
     render() {
         return (
             <div className='text-center'>
-                    <span className="spinner-border text-info" role="status" aria-hidden="true"></span>
+                    <span className="spinner-border text-info my-3" role="status" aria-hidden="true"></span>
                     Loading...
                    
             </div>
