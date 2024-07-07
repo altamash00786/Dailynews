9862c50165d8eafd5a3bc765af9f29424b0fc4d6
Altamash Shaikh <157121783+altamash00786@users.noreply.github.com>
2024-07-07 18:41:42 +0530

    Update NewsItems.js

--- src/components/NewsItems.js
+++ src/components/NewsItems.js
@@ -1,24 +1,28 @@
-import React, { Component } from 'react'
-import { Link } from "react-router-dom";
+import React from 'react';
 
-export class NewsItems extends Component {
-    render() {
-        let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
-        return (
-            <div className="my-3">
-                <div className="card">
-                    <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger" style={{ left: '82%', zIndex: '1' }}>{source}</span>
-                    <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." style={{ width: '358px', height: '200px' }} />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}</h5>
-                        <p className="card-text">{description}</p>
-                        <p class="card-text"><small class="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
-                        <Link rel="noreferrer" to={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More...</Link>
-                    </div>
+const NewsItems = (props) => {
+    let { title, description, imageUrl, newsurl, author, date, source } = props;
+    return (
+        <div className="my-3">
+            <div className="card">
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    position: 'absolute',
+                    right: '0',
+                }}>
+                    <span className="badge rounded-pill bg-success"> {source} </span>
+                </div>
+                <img src={!imageUrl ? "https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg" : imageUrl} className="card-img-top" alt="..." style={{ width: '358px', height: '200px' }} />
+                <div className="card-body">
+                    <h5 className="card-title">{title}  </h5>
+                    <p className="card-text">{description}</p>
+                    <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
+                    <a href={newsurl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read More...</a>
                 </div>
             </div>
-        )
-    }
+        </div>
+    );
 }
 
-export default NewsItems
+export default NewsItems;
